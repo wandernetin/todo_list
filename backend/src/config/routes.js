f639727c4413c29fc1c2c6725fc700a294c6cb7d
Wander <wander.dmneto@gmail.com>
2018-03-04 20:40:28 +1000

    first commit

---
+++ backend/src/config/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+module.exports = function(server) {
+  // API Routers
+  const router = express.Router()
+  server.use('/api', router)
+
+  //Todo routes
+  const todoService = require('../api/todo/todoService')
+  todoService.register(router, '/todos')
+}
